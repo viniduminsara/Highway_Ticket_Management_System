0ccaf9e4624d55c17c2aa8581b4a7259739405f4
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-06-25 15:14:00 +0530

    add config server

---
+++ user-service/src/main/java/lk/ijse/user_service/user_service/controllers/UserController.java
@@ -0,0 +1,16 @@
+package lk.ijse.user_service.user_service.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/user")
+public class UserController {
+
+    @GetMapping
+    public String health(){
+        return "ok";
+    }
+
+}
