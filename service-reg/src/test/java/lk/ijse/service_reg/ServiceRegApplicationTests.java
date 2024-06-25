0d9cc25457a2972e96fba092e63a54db45a16769
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-06-25 10:39:41 +0530

    initialize project

---
+++ service-reg/src/test/java/lk/ijse/service_reg/ServiceRegApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.service_reg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
