9a08bcfa80441fb7cc6a4a3485c6003b400933b9
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-06-25 19:42:51 +0530

    few changes

--- vehicle-service/src/main/java/lk/ijse/vehicle_service/controllers/VehicleController.java
+++ vehicle-service/src/main/java/lk/ijse/vehicle_service/controllers/VehicleController.java
@@ -1,16 +1,16 @@
-package lk.ijse.user_service.user_service.controllers;
+package lk.ijse.vehicle_service.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/users")
-public class UserController {
+@RequestMapping("api/v1/vehicles")
+public class VehicleController {
 
     @GetMapping
     public String health(){
-        return "User Ok";
+        return "Vehicle Ok";
     }
 
 }
