6adba20a20ab92ba24b2597d3dae4b3d41d46ed8
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-06-28 22:19:01 +0530

    create user entity

---
+++ user-service/src/main/java/lk/ijse/user_service/user_service/entity/UserEntity.java
@@ -0,0 +1,31 @@
+package lk.ijse.user_service.user_service.entity;
+
+import jakarta.persistence.*;
+import lk.ijse.user_service.user_service.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userId;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
